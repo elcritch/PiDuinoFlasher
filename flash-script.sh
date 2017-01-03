a5ad362d51a6ccd0d1ab425e169aef6ad1887d41
root <jaremy@brightagrotech.com>
2017-01-03 20:51:05 +0000

    Setup scripts

---
+++ flash-script.sh
@@ -0,0 +1,3 @@
+
+ard-reset-arduino --verbose --caterina /dev/ttyACM0 && sleep 0.3 && avrdude -C ./avrdude.conf -v -p atmega32u4 -c avr109 -P /dev/ttyACM0 -b 57600 -U flash:w:$HEX_FILE:i
+
--- flasher-setup.sh
+++ flasher-setup.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 # wget https://adafruit.github.io/arduino-board-index/package_adafruit_index.json
 
@@ -8,9 +9,10 @@ sudo apt install -y gcc-avr binutils-avr avr-libc avrdude
 sudo apt install -y python-pip
 sudo pip install ino
 
-sudo apt-get install arduino-mk
+sudo apt-get install -y arduino-mk
 
 
-## Working command:
-ard-reset-arduino --verbose --caterina /dev/ttyACM0 && sleep 0.3 && avrdude -C ./avrdude.conf -v -p atmega32u4 -c avr109 -P /dev/ttyACM0 -b 57600 -U flash:w:$HEX_FILE:i
+wget https://adafruit.github.io/arduino-board-index/boards/adafruit-avr-1.4.9.tar.bz2
+
+echo '2fbc90df3cc0b95816ac39da262ddb6eac5c46e47374768bd7de7c99fac1f3dd adafruit-avr-1.4.9.tar.bz2' | sha256sum -c -
 
