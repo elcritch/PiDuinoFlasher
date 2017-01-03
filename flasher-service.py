5488cb5f0eb1b1ed77fc0e06b2c3aa983d64e1b4
root <jaremy@brightagrotech.com>
2017-01-03 23:11:37 +0000

    adding new flasher service code.

--- flasher-service.py
+++ flasher-service.py
@@ -6,15 +6,18 @@ import os.path
 import argparse
 from time import sleep
 import pyudev
+import subprocess
 
 parser = argparse.ArgumentParser(description='Reset an Arduino')
 parser.add_argument('--caterina', action='store_true', help='Reset a Leonardo, Micro, Robot or LilyPadUSB.')
 parser.add_argument('--verbose', action='store_true', help="Watch what's going on on STDERR.")
 parser.add_argument('--period', default=0.1, help='Specify the DTR pulse width in seconds.')
+parser.add_argument('--avrconf', default="/usr/share/arduino/hardware/tools/avrdude.conf", help='AVR Conf File')
+parser.add_argument('firmware', nargs=1, help='Firmware to flash.')
 # parser.add_argument('port', nargs=1, help='Serial device e.g. /dev/ttyACM0')
 args = parser.parse_args()
 
-def flasher(args):
+def flash_reset(args):
     if args.caterina:
         if args.verbose: print('Forcing reset using 1200bps open/close on port %s' % args.port[0])
         ser = serial.Serial(args.port[0], 57600)
@@ -39,6 +42,24 @@ def flasher(args):
         ser.setDTR(True)
         ser.close()
 
+def flash_upload(args):
+    
+    subprocess.call(["avrdude", 
+        "-C", "%s"%(args.avrconf), 
+        "-v", 
+        "-p", "atmega32u4",
+        "-c", "avr109",
+        "-P", "%s"%(args.port[0]),
+        "-b", "57600", 
+        "-U", "flash:w:%s:i"%(args.firmware),
+    ])
+
+def do_flash(args):
+    sleep(1)
+    flash_reset(args)
+    sleep(1)
+    flash_upload(args)
+
 def usb_monitor():
     context = pyudev.Context()
     monitor = pyudev.Monitor.from_netlink(context)
@@ -50,7 +71,8 @@ def usb_monitor():
             print("serial device: ", device.sys_name, device, )
             args.port = [ "/dev/" + device.sys_name ]
             
-            flasher(args)
+            do_flash(args)
+            
 
 
 # ('usb device: ', Device(u'/sys/devices/platform/soc/3f980000.usb/usb1/1-1/1-1.3/1-1.3:1.0/tty/ttyACM0'), u'add')
